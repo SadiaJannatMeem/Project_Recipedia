5261229c5bf13e0dafbd3aa1828a1d5ab7b7a21e
NusaibaBintaKaiser <223011612@eastdelta.edu.bd>
2025-08-02 06:28:11 +0600

    Build added seach bar and heading text

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -8,6 +8,8 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  TextEditingController searchController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,21 +25,71 @@ class _HomeScreenState extends State<HomeScreen> {
                   Color(0xFFa8e063),
                   Color(0xFF56ab2f),
                 ],
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
               ),
             ),
           ),
-          // Foreground content
+
+          // Foreground content inside SafeArea
           SafeArea(
-            child: Center(
-              child: Text(
-                "Hello, I am Home Page",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20,
+            child: Column(
+              children: [
+                const SizedBox(height:30),
+                // Search bar
+                Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 8),
+                  margin: const EdgeInsets.symmetric(horizontal: 24),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(24),
+                  ),
+                  child: Row(
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          if ((searchController.text).replaceAll(" ", "") == "") {
+                            print("Blank search");
+                          } else {
+                            Navigator.pushReplacementNamed(context, "/loading", arguments: {"searchText": searchController.text,
+                              },
+                            );
+                          }
+                        },
+                        child: Container(
+                          margin: const EdgeInsets.fromLTRB(3, 0, 7, 0),
+                          child: const Icon(
+                            Icons.search,
+                            color: Colors.blueAccent,
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: TextField(
+                          controller: searchController,
+                          decoration: const InputDecoration(border: InputBorder.none, hintText: "Let's Cook Something!",
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
+
+                const SizedBox(height: 30), // spacing after the search bar
+
+                // Welcome texts
+                Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: const [
+                      Text("What do you want to cook today?", style: TextStyle(fontSize: 33, color: Colors.white),
+                      ),
+                      SizedBox(height: 10),
+                      Text("Let's Cook Something New!", style: TextStyle(fontSize: 20, color: Colors.white),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
           ),
         ],
